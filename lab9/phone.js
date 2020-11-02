3042d618872d5afa1ee080bb4604706b97264897
Matthew Thien <mthien819@gmail.com>
2020-11-01 19:28:49 -0500

    .

--- lab9/phone.js
+++ lab9/phone.js
@@ -6,14 +6,18 @@ function onInput(ev) {
         return;
     }
 
-    if (ev.target.value.length === 3) { 
-        var value = ev.target.value.concat("-");
-        ev.target.value = value;
+    if (ev.target.value.length === 4) { 
+    	var a = ev.target.value.substr(0, ev.target.value.length - 1)
+        a = a.concat("-");
+        a = a.concat(ev.target.value.substr(3, ev.target.value.length-1));
+        ev.target.value = a;
     }
 
-    if (ev.target.value.length === 7) { 
-        var value = ev.target.value.concat("-");
-        ev.target.value = value;
+    if (ev.target.value.length === 8) { 
+    	var a = ev.target.value.substr(0, ev.target.value.length - 1)
+        a = a.concat("-");
+        a = a.concat(ev.target.value.substr(7, ev.target.value.length-1));
+        ev.target.value = a;
     }
     ev.target.value = ev.target.value.substr(0,12);
 }
